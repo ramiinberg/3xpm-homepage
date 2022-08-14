4540704bc10e831b36c2885c39052e42091205b6
ramiinberg <rami.inberg@gmail.com>
2022-08-14 11:12:08 +0300

    Adding fake ahven data

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -13,11 +13,10 @@ function createData(name, calories, fat, carbs, protein) {
 }
 
 const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9)
+  createData('Wurppe', 28, 1, 28, 25),
+  createData('Timo', 26, 1, 26, 18),
+  createData('Kimmo', 27, 0.75, 20.25, 12),
+  createData('Santeri', 26, 1, 26, 18)
 ]
 
 export default function FishTable() {
