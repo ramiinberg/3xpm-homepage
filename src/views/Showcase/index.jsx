66613635fa532f269274e36051bd4a4ed721469b
ramiinberg <rami.inberg@gmail.com>
2022-10-18 16:42:38 +0300

    pushing small fish image to test

--- src/views/Showcase/index.jsx
+++ src/views/Showcase/index.jsx
@@ -1,13 +1,13 @@
-import perch from '../../images/fish.jpg'
+import smallFish from '../../images/small-fish.jpg'
 import FishCard from '../../components/FishCard'
 import './showcase.scss'
 
 function Showcase() {
   return (
     <div className='showcase'>
-      <FishCard fishImage={perch} fishName='Ahven' />
-      <FishCard fishImage={perch} fishName='Ahven' />
-      <FishCard fishImage={perch} fishName='Ahven' />
+      <FishCard fishImage={smallFish} fishName='Ahven' />
+      <FishCard fishImage={smallFish} fishName='Ahven' />
+      <FishCard fishImage={smallFish} fishName='Ahven' />
     </div>
   )
 }
